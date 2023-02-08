6e1d68d309566bc9164d709f0af05be2121d4c97
Akiomi Kamakura <akiomik@gmail.com>
2023-02-09 05:35:34 +0900

    Add NoteListItemProfile

---
+++ src/components/NoteListItemProfile.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Avatar } from '@skeletonlabs/skeleton';
+  import type { Profile } from '../lib/Profile';
+
+  export let profile: Profile | null;
+</script>
+
+<div class="flex items-center">
+  <div class="mr-2">
+    <Avatar src={profile?.picture} initials="NO" />
+  </div>
+
+  <div class="text-ellipsis overflow-hidden">
+    <p class="font-bold text-ellipsis overflow-hidden">
+      {profile?.displayName || profile?.name || 'nostrich'}
+    </p>
+
+    {#if profile?.nip05}
+      <p class="text-ellipsis overflow-hidden">{profile.nip05}</p>
+    {/if}
+  </div>
+</div>
