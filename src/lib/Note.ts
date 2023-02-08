bea33791ff15acf3c8bf23f0db900f415ed2b2ec
Akiomi Kamakura <akiomik@gmail.com>
2023-02-09 07:44:37 +0900

    Add Note#tags

--- src/lib/Note.ts
+++ src/lib/Note.ts
@@ -2,6 +2,7 @@ import { nip19 } from 'nostr-tools';
 import type { Event } from 'nostr-tools';
 
 import { Profile } from './Profile';
+import { Tag } from './Tag';
 
 export class Note {
   constructor(
@@ -9,17 +10,23 @@ export class Note {
     public content: string,
     public pubkey: string,
     public createdAt: Date,
+    public tags: Tag[],
     public profile: Profile | null,
     public reactions: number
   ) {}
 
   public static fromEvent(note: Event, profileOpt: Event | null, reactions: number): Note {
     const profile = profileOpt == null ? null : Profile.fromEvent(profileOpt);
+    const tags: Tag[] = note.tags
+      .map((tag) => Tag.fromEvent(tag))
+      .filter((tag: Tag | null): tag is Tag => tag !== null);
+
     return new Note(
       note.id,
       note.content,
       note.pubkey,
       new Date(note.created_at * 1000),
+      tags,
       profile,
       reactions
     );
---
+++ src/lib/Tag.ts
@@ -0,0 +1,23 @@
+export class Tag {
+  constructor(public typ: string, public value: string) {}
+
+  public static fromEvent(tag: string[]): Tag | null {
+    if (tag.length < 2) {
+      return null;
+    }
+
+    return new Tag(tag[0], tag[1]);
+  }
+
+  public isEvent(): boolean {
+    return this.typ === 'e';
+  }
+
+  public isPubkey(): boolean {
+    return this.typ === 'p';
+  }
+
+  public isTag(): boolean {
+    return this.typ === 't';
+  }
+}
