42114aeafe1b69e20cd84067d83d32c9d1705e65
Akiomi Kamakura <akiomik@gmail.com>
2023-02-09 05:51:10 +0900

    Add footer to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script>
+  import { AppShell } from '@skeletonlabs/skeleton';
   import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
   import '@skeletonlabs/skeleton/styles/all.css';
   import '../app.postcss';
 </script>
 
-<div class="container mx-auto p-8 space-y-8">
-  <slot />
-</div>
+<AppShell>
+  <div class="container mx-auto p-8 space-y-8">
+    <slot />
+  </div>
+
+  <svelte:fragment slot="footer">
+    <hr />
+
+    <div class="p-8 flex justify-center">
+      <p>&copy; 2023 Nostrends</p>
+    </div>
+  </svelte:fragment>
+</AppShell>
