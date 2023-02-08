0c9ddf9b7f28b2b1207398039a82f71500223674
Akiomi Kamakura <akiomik@gmail.com>
2023-02-09 03:49:05 +0900

    Add more properties to Profile

--- src/lib/Profile.ts
+++ src/lib/Profile.ts
@@ -1,10 +1,24 @@
 import type { Event } from 'nostr-tools';
 
 export class Profile {
-  constructor(public id: string | undefined, public name: string, public pubkey: string) {}
+  constructor(
+    public id: string | undefined,
+    public name: string,
+    public displayName: string,
+    public picture: string,
+    public nip05: string,
+    public pubkey: string
+  ) {}
 
   public static fromEvent(event: Event): Profile {
     const content = JSON.parse(event.content);
-    return new Profile(event.id, content.name, event.pubkey);
+    return new Profile(
+      event.id,
+      content.name,
+      content.display_name,
+      content.picture,
+      content.nip05,
+      event.pubkey
+    );
   }
 }
