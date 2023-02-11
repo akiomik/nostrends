3fb7fb8e6567b230185e373d5947da2841790502
Akiomi Kamakura <akiomik@gmail.com>
2023-02-12 07:01:54 +0900

    Add +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<h1>Nostrends</h1>
+
+<div class="alert variant-ghost-error">
+  <div class="alert-message">
+    <h2>Error</h2>
+    <p>{$page.error?.message}</p>
+  </div>
+</div>
