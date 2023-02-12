ec40fbdb57e9580c80d25dde0ecfcdca41df726c
Akiomi Kamakura <akiomik@gmail.com>
2023-02-12 11:14:25 +0900

    Use primary color

--- src/components/LoadingSpinner.svelte
+++ src/components/LoadingSpinner.svelte
@@ -1,5 +1,5 @@
 <div class="grid h-screen place-items-center">
   <div
-    class="animate-spin h-12 w-12 border-4 border-emerald-500 rounded-full border-t-transparent"
+    class="animate-spin h-12 w-12 border-4 border-primary-500 rounded-full border-t-transparent"
   />
 </div>
