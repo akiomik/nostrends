48f99bf0241e237d3385fc1d43bc5061a67cb097
Akiomi Kamakura <akiomik@gmail.com>
2023-02-14 07:09:13 +0900

    Add NoteLink

---
+++ src/components/NoteLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { settings } from '../stores/settings.ts';
+  import type { Note } from '../lib/Note';
+
+  export let note: Note;
+  export let className = '';
+
+  $: isOpenLinkWithApp = $settings.isOpenLinkWithApp;
+  $: href = isOpenLinkWithApp
+    ? `nostr://${note.nip19Id()}`
+    : `https://snort.social/e/${note.nip19Id()}`;
+</script>
+
+<a {href} target="_blank" rel="noreferrer" class={className}>
+  <slot />
+</a>
