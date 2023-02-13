dcd9cf745255caa7d37961f0e40c47994914a056
Akiomi Kamakura <akiomik@gmail.com>
2023-02-14 06:44:11 +0900

    Add Modal component

---
+++ src/components/Modal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let isOpen: boolean;
+</script>
+
+{#if isOpen}
+  <div
+    role="dialog"
+    class="fixed inset-0 flex justify-center items-center pointer-events-none z-20"
+  >
+    <div class="w-96 p-8 bg-surface-50 rounded pointer-events-auto">
+      <slot />
+    </div>
+  </div>
+{/if}
