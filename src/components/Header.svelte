6bf0d30851f2e1f73e771c42bcdfab6a17e0ce41
Akiomi Kamakura <akiomik@gmail.com>
2023-02-14 07:12:05 +0900

    Add Setting to Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,26 @@
-<script>
+<script lang="ts">
   import { AppBar } from '@skeletonlabs/skeleton';
+  import { openModal } from 'svelte-modals';
+  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+  import { faGear } from '@fortawesome/free-solid-svg-icons';
+  import SettingModal from './SettingModal.svelte';
+
+  const showSettingModal = () => openModal(SettingModal);
 </script>
 
 <AppBar>
-  <a href="/">
-    <span class="flex items-center">
-      <img src="/favicon.svg" alt="logo" class="h-8 mr-1" />
-      Nostrends
-    </span>
-  </a>
+  <svelte:fragment slot="lead">
+    <a href="/">
+      <span class="flex items-center">
+        <img src="/favicon.svg" alt="logo" class="h-8 mr-1" />
+        Nostrends
+      </span>
+    </a>
+  </svelte:fragment>
+
+  <svelte:fragment slot="trail">
+    <button class="btn-icon" on:click={showSettingModal}>
+      <FontAwesomeIcon icon={faGear} />
+    </button>
+  </svelte:fragment>
 </AppBar>
