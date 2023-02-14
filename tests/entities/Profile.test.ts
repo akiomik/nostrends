8cff434df36a1cb995fa9e599b2675e91f56d7cd
Akiomi Kamakura <akiomik@gmail.com>
2023-02-14 09:22:24 +0900

    fixup! Add test for Profile#nip19Id

--- tests/entities/Profile.test.ts
+++ tests/entities/Profile.test.ts
@@ -3,30 +3,6 @@ import type { Event } from 'nostr-tools';
 
 import Profile from '../../src/entities/Profile';
 
-// describe('fromEvent', () => {
-//   it('returns Note', () => {
-//     const event: Event = {
-//       id: 'foobar',
-//       pubkey: 'npub',
-//       content: 'Hello, world!',
-//       kind: 1,
-//       created_at: 1676328835,
-//       tags: [['t', 'nostrends']]
-//     };
-//     const actual = Note.fromEvent(event, null, 42);
-//     const expected = new Note(
-//       'foobar',
-//       'Hello, world!',
-//       'npub',
-//       new Date('2023-02-14T07:53:55+09:00'),
-//       [new Tag('t', 'nostrends')],
-//       null,
-//       42
-//     );
-//     expect(actual).toEqual(expected);
-//   });
-// });
-
 describe('nip19Id', () => {
   // https://github.com/nostr-protocol/nips/blob/master/19.md#examples
   it('returns encoded pubkey according to NIP-19', () => {
